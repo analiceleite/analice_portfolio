310fdadcff7504b720d35c36afe29513e36a1847
Analice Leite <analice.leite12@gmail.com>
2024-09-10 11:04:22 -0300

    removing extra-bold from hero slice

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -71,7 +71,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
         <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
           <Shapes />
           <div className="col-start-1 md:row-start-1" data-speed=".2">
-            <h1 className="mb-8 text-[clamp(3rem,20vmin,40rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
+            <h1 className="mb-8 text-[clamp(3rem,20vmin,40rem)] leading-none tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
               <span className="block text-slate-300">{renderLetters(slice.primary.first_name, "first")}</span>
               <span className="-mt-[.2em] block text-slate-500">{renderLetters(slice.primary.last_name, "last")}</span>
               <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">{slice.primary.tag_line}</span>
