08750680f9bed23c05b646f78c634cbbd7067601
Analice Leite <analice.leite12@gmail.com>
2024-09-09 23:17:15 -0300

    upd page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@ import { SliceZone } from "@prismicio/react";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 
-export default async function Homepage() {
+export default async function Page() {
   const client = createClient();
-  // Obtém a página 'homepage'
   const page = await client.getSingle("homepage");
 
   return (
     <main>
-      {/* Renderiza os slices da página 'homepage' */}
       <SliceZone slices={page.data.slices} components={components} />
     </main>
   );
@@ -21,7 +19,7 @@ export async function generateMetadata(): Promise<Metadata> {
   const page = await client.getSingle("homepage");
 
   return {
-    title: page.data.meta_title || "Analice's Portfólio", 
-    description: page.data.meta_description || "See my portfólio", 
+    title: page.data.meta_title,
+    description: page.data.meta_description,
   };
 }
